3067ef77665ce52b2b785cfd7fce139e4450d523
unknown <nhontqse151350@fpt.edu.vn>
2026-02-03 00:27:05 +0700

    add image

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(cors({
     origin: "*",
     methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"]
+    allowedHeaders: ["Content-Type", "Authorization", "ngrok-skip-browser-warning"]
 }));
 
 const server = http.createServer(app);
